3382d718b206ef29acbd4950abfaed2f087adb4f
Soham Das <sohamdas.nest@gmail.com>
2025-04-20 01:59:22 +0530

    header section and encoding

---
+++ app/dnsMessageHeader.ts
@@ -0,0 +1,61 @@
+export class DNSMessageHeader {
+  constructor(
+    public packetID: number = 0,
+    public isResponse: boolean = false,
+    public opCode: number = 0,
+    public isAuthoritativeAnswer: boolean = false,
+    public isTruncated: boolean = false,
+    public isRecursionDesired: boolean = false,
+    public isRecursionAvailable: boolean = false,
+    public responseCode: number = 0,
+    public questionCount: number = 0,
+    public answerRecordCount: number = 0,
+    public authorityRecordCount: number = 0,
+    public additionalRecordCount: number = 0
+  ) {}
+  encode(): Uint8Array {
+    const encodeBigEndian = (value: number) => {
+      const lowByte = value & 0xff;
+      const highByte = (value >> 8) & 0xff;
+      return [lowByte, highByte];
+    };
+    const byteArray = new Uint8Array(12);
+    // packetID -- FIRST 2 bytes
+    // We get the low and high bytes
+    let [lowByte, highByte] = encodeBigEndian(this.packetID);
+    byteArray[0] = highByte;
+    byteArray[1] = lowByte;
+    // QR, OPCODE, AA, TC, RD -- 3RD BYTE
+    let byte = 0;
+    // if the boolean for is response is true, we do bitwise OR
+    if (this.isResponse) byte |= 0b10000000; // 1 byte (of 8) taken
+    byte |= this.opCode << 3; // 2-5 bytes taken
+    if (this.isAuthoritativeAnswer) byte |= 0b00000100; // 6th
+    if (this.isTruncated) byte |= 0b00000100; // 7th
+    if (this.isRecursionDesired) byte |= 0b00000100; // 8th
+    byteArray[2] = byte;
+    // RA, Z (reserved), RCODE
+    byte = 0;
+    if (this.isRecursionAvailable) byte |= 0b10000000;
+    // reserved is always 0 - reserved for future use
+    byte |= this.responseCode;
+    byteArray[3] = byte;
+    // QDCOUNT
+    [lowByte, highByte] = encodeBigEndian(this.questionCount);
+    byteArray[4] = highByte;
+    byteArray[5] = lowByte;
+    // ANCOUNT
+    [lowByte, highByte] = encodeBigEndian(this.answerRecordCount);
+    byteArray[6] = highByte;
+    byteArray[7] = lowByte;
+    // NSCOUNT
+    [lowByte, highByte] = encodeBigEndian(this.authorityRecordCount);
+    byteArray[8] = highByte;
+    byteArray[9] = lowByte;
+    // NSCOUNT
+    [lowByte, highByte] = encodeBigEndian(this.additionalRecordCount);
+    byteArray[10] = highByte;
+    byteArray[11] = lowByte;
+    return byteArray;
+  }
+}
--- app/main.ts
+++ app/main.ts
@@ -1,19 +1,50 @@
 import * as dgram from "dgram";
+import dnsPacket from "dns-packet"; // 3rd party module to decode and encode DNS packets
+import { DNSMessageHeader } from "./dnsMessageHeader";
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
-// Uncomment this block to pass the first stage
-//
+//socket connection
 const udpSocket: dgram.Socket = dgram.createSocket("udp4");
-udpSocket.bind(2053, "127.0.0.1");
 
-udpSocket.on("message", (data: Buffer, remoteAddr: dgram.RemoteInfo) => {
-    try {
-        console.log(`Received data from ${remoteAddr.address}:${remoteAddr.port}`);
-        const response = Buffer.from("");
-        udpSocket.send(response, remoteAddr.port, remoteAddr.address);
-    } catch (e) {
-        console.log(`Error sending data: ${e}`);
-    }
+udpSocket.bind(2053, "127.0.0.1", () => {
+  console.log("bind done!");
 });
+
+udpSocket.on("listening", () => {
+  const address = udpSocket.address();
+  console.log(`UDP Server listening on ${address.address}:${address.port}`);
+});
+
+udpSocket.on("error", (err) => {
+  console.log(`server error:\n${err.stack}`);
+  udpSocket.close();
+});
+
+udpSocket.on("close", () => {
+  console.log("server closed");
+});
+
+// dns default header
+const defaultHeader = new DNSMessageHeader();
+defaultHeader.packetID = 1234;
+defaultHeader.isResponse = true;
+
+// receive data
+udpSocket.on("message", (data: Buffer, remoteAddr: dgram.RemoteInfo) => {
+  try {
+    const DecodedPacket = dnsPacket.decode(data);
+    console.log(`Received data from ${remoteAddr.address}:${remoteAddr.port}`);
+    console.log("raw packet : ", data);
+    console.log("decoded packet : ", DecodedPacket);
+
+    const response = Buffer.from(defaultHeader.encode());
+    // send data
+    udpSocket.send(response, remoteAddr.port, remoteAddr.address);
+  } catch (e) {
+    console.log(`Error sending data: ${e}`);
+  }
+});
+
+/*
+ * Use dig command to test the server
+ * dig @127.0.0.1 -p 2053 google.com
+ */
